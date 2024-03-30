f82b4e6d5fb3da12cd9430ddf23e503fb709119a
Firo91 <112562425+Firo91@users.noreply.github.com>
2024-03-30 19:53:52 +0100

    Update settings.py

--- MessageAppPWA/settings.py
+++ MessageAppPWA/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY", default="change_me")
 
-DEBUG = env("DEBUG", default=False)
+DEBUG = env("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
@@ -86,7 +86,7 @@ DATABASES = {
     # ImproperlyConfigured exception if not found
     #
     # The db() method is an alias for db_url().
-    "default": env.db()
+    "default": env.db(default="sqlite:///db.sqlite3"),
 }
 
 if DATABASES["default"]["ENGINE"] == "django.db.backends.postgresql":
